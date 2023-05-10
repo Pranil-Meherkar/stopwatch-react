9aff67b28eb70ec80cda8788920ab517548a4a9c
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-05-10 11:11:32 +0530

    updated meta tag

--- src/components/MetaDecorator.jsx
+++ src/components/MetaDecorator.jsx
@@ -10,7 +10,7 @@ const MetaDecorator = ({ title, description, imageUrl, imageAlt }) => (
     <meta property="og:image" content={"https://picsum.photos/200/300"} />
     <meta
       property="og:url"
-      content={"https://stopwatch-react-nine.vercel.app/" + window.location.pathname + window.location.search}
+      content={"https://stopwatch-react-nine.vercel.app" + window.location.pathname + window.location.search}
     />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image:alt" content={imageAlt} />
